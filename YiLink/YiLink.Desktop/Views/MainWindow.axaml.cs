0083980be0655b2d2acd63a72c4192dc26b29639
lingyicute <160479206+lingyicute@users.noreply.github.com>
2025-07-07 10:45:56 +0000

    no promotion

--- YiLink/YiLink.Desktop/Views/MainWindow.axaml.cs
+++ YiLink/YiLink.Desktop/Views/MainWindow.axaml.cs
@@ -33,7 +33,6 @@ public partial class MainWindow : WindowBase<MainWindowViewModel>
 
         this.KeyDown += MainWindow_KeyDown;
         menuSettingsSetUWP.Click += menuSettingsSetUWP_Click;
-        menuPromotion.Click += menuPromotion_Click;
         menuCheckUpdate.Click += MenuCheckUpdate_Click;
         menuBackupAndRestore.Click += MenuBackupAndRestore_Click;
         menuClose.Click += MenuClose_Click;
@@ -331,11 +330,6 @@ public partial class MainWindow : WindowBase<MainWindowViewModel>
                 ViewModel?.Reload();
             }
         }
-    }
-
-    private void menuPromotion_Click(object? sender, RoutedEventArgs e)
-    {
-        ProcUtils.ProcessStart($"{Utils.Base64Decode(Global.PromotionUrl)}?t={DateTime.Now.Ticks}");
     }
 
     private void menuSettingsSetUWP_Click(object? sender, RoutedEventArgs e)
--- YiLink/YiLink/Views/MainWindow.xaml.cs
+++ YiLink/YiLink/Views/MainWindow.xaml.cs
@@ -30,7 +30,6 @@ public partial class MainWindow
         this.Closing += MainWindow_Closing;
         this.PreviewKeyDown += MainWindow_PreviewKeyDown;
         menuSettingsSetUWP.Click += menuSettingsSetUWP_Click;
-        menuPromotion.Click += menuPromotion_Click;
         menuClose.Click += menuClose_Click;
         menuCheckUpdate.Click += MenuCheckUpdate_Click;
         menuBackupAndRestore.Click += MenuBackupAndRestore_Click;
@@ -320,11 +319,6 @@ public partial class MainWindow
     {
         StorageUI();
         ShowHideWindow(false);
-    }
-
-    private void menuPromotion_Click(object sender, RoutedEventArgs e)
-    {
-        ProcUtils.ProcessStart($"{Utils.Base64Decode(Global.PromotionUrl)}?t={DateTime.Now.Ticks}");
     }
 
     private void menuSettingsSetUWP_Click(object sender, RoutedEventArgs e)
